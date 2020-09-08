d5e29f558ae3a69df959e63e65d5ee526ae3bff3
LuckyMasharane <lmasharane@gmail.com>
2020-09-08 10:41:43 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {Routes, RouterModule } from '@angular/router';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
@@ -13,8 +14,14 @@ import { HelloComponent } from './hello.component';
 import { NewEmpComponent } from './new-emp/new-emp.component';
 import { DialogComponent } from './dialog/dialog.component';
 
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'employee', component: NewEmpComponent },
+];
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,MatCardModule, MatButtonModule, MatGridListModule ],
+  imports:      [ BrowserModule, FormsModule ,MatCardModule, MatButtonModule, MatGridListModule,
+      RouterModule.forRoot(routes) ],
   declarations: [ AppComponent, HelloComponent, DialogComponent, NewEmpComponent ],
   entryComponents:[ DialogComponent ],
   bootstrap:    [ AppComponent ],
