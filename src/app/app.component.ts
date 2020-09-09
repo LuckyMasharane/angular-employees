2611b973d81cec81007e8cdb8e44b1bd8db46a9e
LuckyMasharane <lmasharane@gmail.com>
2020-09-09 11:10:07 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { DialogComponent } from './dialog/dialog.component';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
 
-  constructor(public dialog: MatDialog) {}
+  // constructor(public dialog: MatDialog) {}
 
   employees =[{empName:"Vusi",empSurname:"Ngomane",empPosition:"front-end Developer",empAge:19,empImage:"https://images.pexels.com/photos/1648413/pexels-photo-1648413.jpeg?cs=srgb&dl=pexels-victoria-borodinova-1648413.jpg&fm=jpg"},{empName:"Lucky",empSurname:"Ndlami",empPosition:"Developer",empAge:20,empImage:"https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?cs=srgb&dl=pexels-justin-shaifer-1222271.jpg&fm=jpg"},
   {empName:"December",empSurname:"Makhanya",empPosition:"Mobile Developer",empAge:24,empImage:"https://images.pexels.com/photos/3775534/pexels-photo-3775534.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-3775534.jpg&fm=jpg"},
@@ -23,24 +23,24 @@ export class AppComponent  {
   {empName:"Thabo",empSurname:"Mbatha",empPosition:"Senior Developer",empAge:27,empImage:"https://images.pexels.com/photos/1445527/pexels-photo-1445527.jpeg?cs=srgb&dl=pexels-doug-bolton-1445527.jpg&fm=jpg"}]
 
    
-  openDialog(employees) {
-    const dialogRef = this.dialog.open(DialogComponent,{
-      data:{
-        message: 'Are you sure want to delete?',
-        buttonText: {
-          ok: 'Delete',
-          cancel: 'No'
-        }
-      }
-    });
+  // openDialog(employees) {
+  //   const dialogRef = this.dialog.open(DialogComponent,{
+  //     data:{
+  //       message: 'Are you sure want to delete?',
+  //       buttonText: {
+  //         ok: 'Delete',
+  //         cancel: 'No'
+  //       }
+  //     }
+  //   });
 
-    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
-      if (confirmed) {
-        const index = this.employees.indexOf(employees,0)
-        if (index >-1) {
-          this.employees.splice(index,1);
-        }
-      }
-    });
-  }
+  //   dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+  //     if (confirmed) {
+  //       const index = this.employees.indexOf(employees,0)
+  //       if (index >-1) {
+  //         this.employees.splice(index,1);
+  //       }
+  //     }
+  //   });
+  // }
 }
